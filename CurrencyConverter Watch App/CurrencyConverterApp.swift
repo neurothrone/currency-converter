c9fd1aa0064a9e7c6f8bbc75a68ffe6c8542661d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-09 16:38:36 +0200

    Initial Commit

---
+++ CurrencyConverter Watch App/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-09.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
